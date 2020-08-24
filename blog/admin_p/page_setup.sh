58f831f225b333427db35841169270883baa98fd
arbaza <55850904+arbaza@users.noreply.github.com>
2020-08-24 09:25:27 -0700

    Added instructions to add an admin page

---
+++ blog/admin_p/page_setup.sh
@@ -0,0 +1,13 @@
+# python manage.py createsuperuser
+#This command will give an error as the admin page is not setup already. We will have to add an admin to a database first.
+
+#python manage.py makemigrations
+#This command will return 'No changes detected', because no changes have been made yet.
+
+#python manage.py migrate
+#This will create an admin with a bunch of OK
+
+#python manage.py createsuperuser
+#This command will create the super user for the admin page. Enter the username, email, and password to setup the suoer user
+
+#Next go to the server that is running and route to the admin page. There you can add users, edit information, etc.
