2574edc1f15b17f12c9ae8fec17092cbe42c2e40
arbaza <55850904+arbaza@users.noreply.github.com>
2020-08-24 10:38:56 -0700

    Added post to the database editable by the admin in GUI

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+#python manage.py makemigrations
+#python manage.py migrate
+    
+#Creating a post in the database through django shell
+# python manage.py shell
+# from blog.models import Post
+# from django.contrib.auth.models import User
+# User.objects.all()
+# User.objects.first()
+# User.objects.filter(username='arbaza')
+# user = User.objects.filter(username='arbaza').first()
+# user.id or user.pk
+# Post.objects.all() check post
+# post_1 = Post(title = 'Blog 1', content = 'First Post Content!', author = user)
+# post_1.save()
+# user.post_set.all()
+
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse
 
 # Create your views here.
 
 
-posts = [
-    {
-        'author': 'Arbaz Ahmed',
-        'title': 'Post number 1',
-        'content': 'Post goes here',
-        'date_posted': 'June 16, 2020'
-    },
-    {
-        'author': 'Archit Rathi',
-        'title': 'Post number 2',
-        'content': 'Post goes here for rathi',
-        'date_posted': 'June 20, 2020'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Arbaz Ahmed',
+#         'title': 'Post number 1',
+#         'content': 'Post goes here',
+#         'date_posted': 'June 16, 2020'
+#     },
+#     {
+#         'author': 'Archit Rathi',
+#         'title': 'Post number 2',
+#         'content': 'Post goes here for rathi',
+#         'date_posted': 'June 20, 2020'
+#     }
+# ]
+
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
